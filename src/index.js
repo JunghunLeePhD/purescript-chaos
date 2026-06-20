65ca84419362e6c330df5653490d11692da6d2ff
junghun <yijeonghunxd@gmail.com>
2026-06-20 09:44:02 +0000

    index/JuliaSet: initialize - index.html - index.js - style.css

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { initializeTodo } from "../output/Main/index.js";
-
-class TodoApp extends HTMLElement {
-  connectedCallback() {
-    initializeTodo(this)();
-  }
-}
-
-customElements.define("todo-app", TodoApp);
