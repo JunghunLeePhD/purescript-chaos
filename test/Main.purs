b8e25ab8252088f32a945b4fad60eb1b5047792e
junghun <yijeonghunxd@gmail.com>
2026-06-22 13:36:27 +0000

    test/JuliaSet: update act and escapeTime - add complex numbers

--- test/Main.purs
+++ test/Main.purs
@@ -13,63 +13,94 @@ import Data.List.Lazy
   , fromFoldable
   , takeWhile
   )
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe)
 import Data.Monoid.Endo (Endo(..))
 import Data.Newtype (unwrap)
+import Data.Number (sqrt)
 import Effect (Effect)
 import Effect.Console (log)
 
-type EndoReal = Endo (->) Number
+class Ring a <= NormedRing a where
+  norm :: a -> Number
 
-quadratic :: Number -> EndoReal
-quadratic c = Endo (\z -> z * z + c)
-
-cyclic :: EndoReal -> List EndoReal
-cyclic f = repeat f
+cyclic :: forall a. Endo (->) a -> List (Endo (->) a)
+cyclic = repeat
 
 class PolymorphicAction actor target result | actor target -> result where
   act :: actor -> target -> result
 
-instance actEndoNum :: PolymorphicAction EndoReal Number Number where
+instance actEndoAny :: PolymorphicAction (Endo (->) a) a a where
   act = unwrap
 
-instance actListEndoNum :: PolymorphicAction (List EndoReal) Number (List Number) where
+else instance actListEndoAny :: PolymorphicAction (List (Endo (->) a)) a (List a) where
   act fs z = scanl (\acc f -> act f acc) z fs
 
-instance actListTarget ::
+else instance actListTarget ::
   ( PolymorphicAction actor target result
   ) =>
   PolymorphicAction actor (List target) (List result) where
   act = map <<< act
 
-escapeTime :: (Number -> Boolean) -> List EndoReal -> Number -> Maybe Int
+class PolymorphicActionE actor target result element | actor target -> result element where
+  actE :: (element -> Boolean) -> actor -> target -> result
+
+instance actEListEndoNormed :: NormedRing a => PolymorphicActionE (List (Endo (->) a)) a (List a) a where
+  actE isOk fs z = takeWhile isOk (act fs z)
+
+else instance actEListTarget ::
+  ( PolymorphicActionE actor target result element
+  ) =>
+  PolymorphicActionE actor (List target) (List result) element where
+  actE isOk fs zs = (actE isOk fs) <$> zs
+
+escapeTime
+  :: forall actor target
+   . (PolymorphicAction (List actor) target (List target))
+  => (target -> Boolean)
+  -> List actor
+  -> target
+  -> Maybe Int
 escapeTime isOk fs z =
   findIndex (not <<< isOk) (act fs z)
 
-class (PolymorphicAction actor target result) <= PolymorphicActionE actor target result | actor target -> result where
-  actE :: (Number -> Boolean) -> actor -> target -> result
+data Complex = Complex Number Number
 
-instance actEListEndoNum :: PolymorphicActionE (List EndoReal) Number (List Number) where
-  actE isOk fs z = takeWhile isOk (act fs z)
+derive instance eqComplex :: Eq Complex
+instance showComplex :: Show Complex where
+  show (Complex r i) = show r <> " + " <> show i <> "i"
 
-instance actEListEndoListNum :: PolymorphicActionE (List EndoReal) (List Number) (List (List (Number))) where
-  actE isOk fs zs = (actE isOk fs) <$> zs
+instance semiringComplex :: Semiring Complex where
+  add (Complex r1 i1) (Complex r2 i2) = Complex (r1 + r2) (i1 + i2)
+  zero = Complex 0.0 0.0
+  mul (Complex r1 i1) (Complex r2 i2) = Complex (r1 * r2 - i1 * i2) (r1 * i2 + r2 * i1)
+  one = Complex 1.0 0.0
 
-colorPixel :: Maybe Int -> String
-colorPixel Nothing = "#000000"
-colorPixel (Just n) = "hsl(" <> show (n * 5) <> ", 100%, 50%)"
+instance ringComplex :: Ring Complex where
+  sub (Complex r1 i1) (Complex r2 i2) = Complex (r1 - r2) (i1 - i2)
+
+instance normedRingComplex :: NormedRing Complex where
+  norm (Complex r i) = sqrt (r * r + i * i)
+
+type EndoComplex = Endo (->) Complex
+
+quadratic :: Complex -> EndoComplex
+quadratic c = Endo $ \z -> z * z + c
+
+-- colorPixel :: Maybe Int -> String
+-- colorPixel Nothing = "#000000"
+-- colorPixel (Just n) = "hsl(" <> show (n * 5) <> ", 100%, 50%)"
 
 main :: Effect Unit
 main = do
   let
-    base = quadratic 0.0
+    base = quadratic (Complex 0.0 0.0)
     -- endos = base
-    endos = take 100 (cyclic base)
-    -- pts = 1.1
-    pts = fromFoldable [ 1.02, 1.03 ]
-  -- log $ show $ act endos pts
+    endos = take 10 (cyclic base)
+    -- pts = Complex 0.0 1.02
+    pts = fromFoldable [ Complex 0.0 0.9, Complex 0.0 1.1 ]
+  log $ show $ act endos pts
   log $ show $ escapeTime isInside endos <$> pts
   log $ show $ actE isInside endos pts
   where
-  isInside :: Number -> Boolean
-  isInside = \z -> z * z < 4.0
+  isInside :: forall a. NormedRing a => a -> Boolean
+  isInside = \z -> norm z < 4.0
