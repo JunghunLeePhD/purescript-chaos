91044cff0b31db52c1bb2a62cf9930cb121d2214
junghun <yijeonghunxd@gmail.com>
2026-06-23 10:45:37 +0000

    Main: update main - remove 'Fractal'

--- src/Main.purs
+++ src/Main.purs
@@ -2,21 +2,23 @@ module Main where
 
 import Prelude
 
+import Control.Monad.Except (runExceptT, except)
+import Control.Monad.Reader (runReader)
+import Control.Monad.Trans.Class (lift)
 import Data.Array ((..))
-import Data.Int (floor)
 import Data.Either (Either(..), note)
 import Data.Foldable (foldM, traverse_)
+import Data.Int (floor, toNumber)
+import Data.List.Lazy (take, repeat)
+import Data.Monoid.Endo (Endo(..))
+import Data.Time.Duration (Milliseconds(..))
 import Effect (Effect)
 import Effect.Aff (delay, launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Console (log)
-import Control.Monad.Except (runExceptT, except)
-import Control.Monad.Trans.Class (lift)
-import Data.Time.Duration (Milliseconds(..))
 import Graphics.Canvas (getCanvasElementById, getContext2D, setFillStyle, fillRect, getCanvasWidth, getCanvasHeight)
-
-import Fractal.Types (Complex(..), Screen(..))
-import Fractal.Algorithm (JuliaSet(..), calculatePixel)
+import JuliaSet.Figure (Pixel(..), PixelWithColor(..), Plane(..), Screen(..), getPixelWithColor)
+import Types.NRing (Complex(..))
 
 main :: Effect Unit
 main = launchAff_ do
@@ -26,40 +28,49 @@ main = launchAff_ do
     canvas <-
       except $ note "Canvas element 'juliaCanvas' not found!" mCanvas
 
-    ctx <- lift $ liftEffect $ getContext2D canvas
-    wNum <- lift $ liftEffect $ getCanvasWidth canvas
-    hNum <- lift $ liftEffect $ getCanvasHeight canvas
+    ctx <-
+      lift $ liftEffect $ getContext2D canvas
+    wNum <-
+      lift $ liftEffect $ getCanvasWidth canvas
+    hNum <-
+      lift $ liftEffect $ getCanvasHeight canvas
 
     let
       width = floor wNum
       height = floor hNum
-      screen = Screen width height
-      viewPlane =
-        { xMin: -1.5
-        , xMax: 1.5
-        , yMin: -1.5
-        , yMax: 1.5
+
+      renderEnv =
+        { screen:
+            Screen width height
+        , plane:
+            Plane (-1.5) 1.5 (-1.5) 1.5
         }
-      maxIter = 100
-      myFractal = JuliaSet (Complex (-0.8) 0.156)
-      processCoord = calculatePixel screen viewPlane maxIter myFractal
+      fs =
+        take 400
+          $ repeat
+          $
+            Endo (\z -> z * z + (Complex (-0.8) 0.156))
 
     lift $ liftEffect $ log "Painting fractal row-by-row functionally..."
 
     lift $ foldM
-      ( \_ px -> do
-          let rowPixels = processCoord px <$> (0 .. width)
+      ( \_ py -> do
+          let
+            rowPixels =
+              (\px -> runReader (getPixelWithColor fs (Pixel px py)) renderEnv) <$> (0 .. width)
+
           liftEffect $ traverse_
-            ( \{ x, y, color } -> do
+            ( \(PixelWithColor curPx curPy color) -> do
                 setFillStyle ctx color
                 fillRect ctx
-                  { x
-                  , y
+                  { x: toNumber curPx
+                  , y: toNumber curPy
                   , width: 1.0
                   , height: 1.0
                   }
             )
             rowPixels
+
           delay $ Milliseconds 0.0
       )
       unit
