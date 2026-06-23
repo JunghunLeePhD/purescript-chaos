cdeef60fcbf439dc2242f192c78846d959f03473
junghun <yijeonghunxd@gmail.com>
2026-06-23 09:17:10 +0000

    Algorithm/JuliaSet: update getEscapeTime

--- src/JuliaSet/Algorithm.purs
+++ src/JuliaSet/Algorithm.purs
@@ -5,23 +5,19 @@ import Prelude
 import Data.List.Lazy
   ( List
   , findIndex
-  , repeat
-  , take
   )
 import Data.Maybe (Maybe(..))
-import Data.Monoid.Endo (Endo(..))
 import Types.Action
-  ( class PolymorphicAction
-  , act
+  ( act
   , EndoComplex
   )
 import Types.NRing (Complex, norm)
 
-isBounded :: Complex -> Boolean
-isBounded z = norm z < 4.0
-
 getEscapeTime :: List EndoComplex -> Complex -> Maybe Int
 getEscapeTime fs z = findIndex (not <<< isBounded) (act fs z)
+  where
+  isBounded :: Complex -> Boolean
+  isBounded z' = norm z' < 4.0
 
 getColor :: List EndoComplex -> Complex -> String
 getColor fs z = orbitToColor $ getEscapeTime fs z
