7516778f919fd8f58851aaadd94fffe536704f76
junghun <yijeonghunxd@gmail.com>
2026-06-23 09:15:14 +0000

    Algorithm/JuliaSet: remove 'escapeTime' - remove escapeTimeOfJuliaSet - remove getColorOfJuliaSet - remove quadraticF

--- src/JuliaSet/Algorithm.purs
+++ src/JuliaSet/Algorithm.purs
@@ -17,22 +17,6 @@ import Types.Action
   )
 import Types.NRing (Complex, norm)
 
-quadraticF :: Complex -> List EndoComplex
-quadraticF c = repeat $ quadratic c
-  where
-  quadratic :: Complex -> EndoComplex
-  quadratic c' = Endo $ \z -> z * z + c'
-
-escapeTime
-  :: forall actor target
-   . (PolymorphicAction (List actor) target (List target))
-  => (target -> Boolean)
-  -> List actor
-  -> target
-  -> Maybe Int
-escapeTime isOk fs z =
-  findIndex (not <<< isOk) (act fs z)
-
 isBounded :: Complex -> Boolean
 isBounded z = norm z < 4.0
 
@@ -41,21 +25,7 @@ getEscapeTime fs z = findIndex (not <<< isBounded) (act fs z)
 
 getColor :: List EndoComplex -> Complex -> String
 getColor fs z = orbitToColor $ getEscapeTime fs z
-  where 
-    orbitToColor :: Maybe Int -> String
-    orbitToColor Nothing = "#000000"
-    orbitToColor (Just n') = "hsl(" <> show (n' * 5) <> ", 100%, 50%)"
-
-escapeTimeOfJuliaSet :: Int -> Complex -> Complex -> Maybe Int
-escapeTimeOfJuliaSet n c z =
-  escapeTime isBounded (quadraticFF n c) z
   where
-  quadraticFF :: Int -> Complex -> List EndoComplex
-  quadraticFF n' c' = take n' $ quadraticF c'
-
-getColorOfJuliaSet :: Int -> Complex -> Complex -> String
-getColorOfJuliaSet n c z = getColor $ escapeTimeOfJuliaSet n c z
-  where
-  getColor :: Maybe Int -> String
-  getColor Nothing = "#000000"
-  getColor (Just n') = "hsl(" <> show (n' * 5) <> ", 100%, 50%)"
+  orbitToColor :: Maybe Int -> String
+  orbitToColor Nothing = "#000000"
+  orbitToColor (Just n') = "hsl(" <> show (n' * 5) <> ", 100%, 50%)"
