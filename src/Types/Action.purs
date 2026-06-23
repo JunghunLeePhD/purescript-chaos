6f584a31054776e2beb7e5fbd6b95907819782ae
junghun <yijeonghunxd@gmail.com>
2026-06-23 01:10:44 +0000

    types/JuliaSet: add action - add polymorphic action - add efficient polymorphic action

---
+++ src/Types/Action.purs
@@ -0,0 +1,32 @@
+module Types.Action where
+
+import Prelude
+
+import Data.List.Lazy
+  ( List
+  , scanl
+  , takeWhile
+  )
+import Data.Monoid.Endo (Endo)
+import Data.Newtype (unwrap)
+
+class PolymorphicAction actor target result | actor target -> result where
+  act :: actor -> target -> result
+
+instance actEndoAny :: PolymorphicAction (Endo (->) a) a a where
+  act = unwrap
+
+else instance actListEndoAny :: PolymorphicAction (List (Endo (->) a)) a (List a) where
+  act fs z = scanl (\acc f -> act f acc) z fs
+
+else instance actListTarget ::
+  ( PolymorphicAction actor target result
+  ) =>
+  PolymorphicAction actor (List target) (List result) where
+  act = map <<< act
+
+class PolymorphicActionE actor target result element | actor target -> result element where
+  actE :: (element -> Boolean) -> actor -> target -> result
+
+instance actEListEndoNormed :: PolymorphicActionE (List (Endo (->) a)) a (List a) a where
+  actE isOk fs z = takeWhile isOk (act fs z)
