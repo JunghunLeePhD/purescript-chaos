bd584bd6a716dbbed9d4fbb9cfc6903e7de26217
junghun <yijeonghunxd@gmail.com>
2026-07-07 00:01:39 +0000

    Main/JuliaSet: add Space - add normedRing - add Real - add Complex

---
+++ src/JuliaSet/Space.purs
@@ -0,0 +1,31 @@
+module JuliaSet.NormedRing where
+
+import Prelude
+import Data.Number (sqrt)
+
+class Ring a <= NormedRing a where
+  norm :: a -> Number
+
+type Real = Number
+data Complex = Complex Real Real
+
+derive instance eqComplex :: Eq Complex
+instance showComplex :: Show Complex where
+  show (Complex r i) = show r <> " + " <> show i <> "i"
+
+instance semiringComplex :: Semiring Complex where
+  add (Complex r1 i1) (Complex r2 i2) = Complex (r1 + r2) (i1 + i2)
+  zero = Complex 0.0 0.0
+  mul (Complex r1 i1) (Complex r2 i2) = Complex (r1 * r2 - i1 * i2) (r1 * i2 + r2 * i1)
+  one = Complex 1.0 0.0
+
+instance ringComplex :: Ring Complex where
+  sub (Complex r1 i1) (Complex r2 i2) = Complex (r1 - r2) (i1 - i2)
+
+instance normedRingReal :: NormedRing Real where
+  norm x
+    | x < 0.0 = -x
+    | otherwise = x
+
+instance normedRingComplex :: NormedRing Complex where
+  norm (Complex r i) = sqrt (norm r + norm i)
