a3f4e4d0c2ed691d89bd3cbe7f1135082901e2eb
junghun <yijeonghunxd@gmail.com>
2026-07-07 00:04:41 +0000

    Main/JuliaSet: add Endomorphism - add polymorphic action

---
+++ src/JuliaSet/Endomorphism.purs
@@ -0,0 +1,23 @@
+module JuliaSet.Endomorphism where
+
+import Prelude
+import Data.Maybe (Maybe)
+import Data.Monoid.Endo (Endo)
+import Data.Newtype (unwrap)
+import Data.List.Lazy (List, scanl)
+
+class PolymorphicAction actor target result | actor target -> result where
+  act :: actor -> target -> result
+
+instance actEndoAny :: PolymorphicAction (Endo (->) a) a a where
+  act = unwrap
+
+else instance actMaybeTarget ::
+  ( PolymorphicAction actor target result
+  ) =>
+  PolymorphicAction actor (Maybe target) (Maybe result) where
+  act actor maybeTarget = act actor <$> maybeTarget
+
+else instance actListEndoAny ::
+  PolymorphicAction (List (Endo (->) a)) a (List a) where
+  act fs z = scanl (\acc f -> act f acc) z fs
