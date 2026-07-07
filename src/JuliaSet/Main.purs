6c957acd2bc8c1e4c3e2e5db5ba8aee8fecae710
junghun <yijeonghunxd@gmail.com>
2026-07-07 00:26:55 +0000

    Main/JuliaSet: add getComplex - add affine - add normalizer

--- src/JuliaSet/Main.purs
+++ src/JuliaSet/Main.purs
@@ -4,7 +4,7 @@ import Prelude
 
 import Control.Monad.Except (runExceptT, except)
 import Data.Either (Either(..), note)
-import Data.Int (floor)
+import Data.Int (floor, toNumber)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
@@ -17,14 +17,28 @@ import Graphics.Canvas
   )
 
 import Data.Array ((..))
-import Data.List.Lazy (List, fromFoldable)
+import Data.Maybe (Maybe(..))
+import Data.Monoid.Endo (Endo(..))
+import Data.List.Lazy (List, fromFoldable, (!!), zipWith)
 import Data.Traversable (traverse)
+
+import JuliaSet.Space (Real, Complex(..))
+import JuliaSet.Endomorphism (act)
 
 type Screen = List Int
 type Pixel = List Int
+type EndoReal = Endo (->) Real
+type EndoComplex = Endo (->) Complex
 
 generatePixel :: Screen -> List Pixel
 generatePixel = traverse (\d -> fromFoldable (0 .. d))
+
+affine :: Real -> Real -> EndoReal
+affine a b = Endo $ \x -> a * x + b
+
+getComplex :: List Real -> Maybe Complex
+getComplex lazyList =
+  Complex <$> (lazyList !! 0) <*> (lazyList !! 1)
 
 main :: Effect Unit
 main = launchAff_ do
@@ -41,6 +55,17 @@ main = launchAff_ do
         -- Screen -> [Pixel]
         screen = fromFoldable [ width, height ]
         pixels = generatePixel screen
+        magnitude = 0.01
+
+        normalizer :: List EndoReal
+        normalizer = fromFoldable
+          [ affine (1.0 / (magnitude * toNumber width)) (-0.5 / magnitude)
+          , affine (1.0 / (magnitude * toNumber height)) (-0.5 / magnitude)
+          ]
+
+        cpxs =
+          (getComplex <<< (zipWith act normalizer) <<< map toNumber) <$> pixels
+
       pure $ unit
     pure $ unit
 
